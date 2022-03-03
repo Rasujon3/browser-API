dfc01d1790304845b24dc1d4c9854fc7c5b599d9
Rasujon3 <rasujon3@gmail.com>
2022-03-04 02:02:58 +0600

    39-8 (advanced) retrieve local storage value and display them

--- shopping.js
+++ shopping.js
@@ -1,12 +1,23 @@
+const displayLocalStorageCart = () => {
+  const cart = getCart();
+  for (const name in cart) {
+    displayProduct(name);
+  }
+};
+
 const addItem = () => {
   const nameField = document.getElementById("product-name");
   const name = nameField.value;
+  if (!name) {
+    return;
+  }
   // display in the UI
   displayProduct(name);
+
   // add to local storage
   addProductToCart(name);
 
-  //   field emply
+  //   clear
   nameField.value = "";
 };
 
@@ -30,7 +41,18 @@ const getCart = () => {
 
 const addProductToCart = (name) => {
   const cart = getCart();
-  cart[name] = 1;
+  if (cart[name]) {
+    cart[name] = cart[name] + 1;
+  } else {
+    cart[name] = 1;
+  }
   const cartStringified = JSON.stringify(cart);
   localStorage.setItem("cart", cartStringified);
 };
+
+const placeOrder = () => {
+  document.getElementById("products").textContent = "";
+  localStorage.removeItem("cart");
+};
+
+displayLocalStorageCart();

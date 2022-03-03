3b1d0756041902c223e7cb6ddd22bee1ba07c549
Rasujon3 <rasujon3@gmail.com>
2022-03-03 22:40:36 +0600

    DOM vs BOM

---
+++ explore.js
@@ -0,0 +1,15 @@
+const name = 15;
+const student = {
+  name: "Jolil",
+  friend: "borsha",
+  job: "business",
+  makeMovie: function (name) {
+    console.log(name);
+  },
+};
+student.makeMovie("Din rat");
+// document.getElementById("name");
+const url = `http://google.com`;
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => console.log(data));
